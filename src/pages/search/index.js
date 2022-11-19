77d11a02e0ddebfcb19b447233d0032d774d7ff5
vickomaris <64400928+vickomaris@users.noreply.github.com>
2022-11-19 18:20:18 +0700

    search page

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,9 +1,38 @@
 import React from 'react'
 import styles from "./search.module.css"
 
+import Navbar from '../../component/module/navbarLogin'
+import CardProduct from '../../component/module/cardProduct'
+
 const Search = () => {
   return (
-    <div>Search Page</div>
+    <>
+      <Navbar/>
+      <section className={`mt-5 ${styles.main}`}>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 py-5">
+              <div className="d-flex flex-row">
+                <p className={`me-3 ${styles.textInfocategory}`}>Home</p>
+                <p className={`me-3 ${styles.textInfocategory}`}> > </p>
+                <p className={`me-3 ${styles.textInfocategory}`}>Category</p>
+                <p className={`me-3 ${styles.textInfocategory}`}> > </p>
+                <p className={`me-3 ${styles.textInfocategory}`}>T-Shirt</p>
+              </div>
+            </div>
+            <p className={`mt-3 ${styles.textTitleCategory}`}> T-Shirt </p>
+            <div class="row row-cols-1 row-cols-md-5 g-4">
+              <CardProduct/>
+              <CardProduct/>
+              <CardProduct/>
+              <CardProduct/>
+              <CardProduct/>
+              <CardProduct/>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
