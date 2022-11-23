131ab9af1014e6a7816e59c854515e73fa2bb78d
Brandon Wijaya <brndnwjy8@gmail.com>
2022-11-23 15:54:47 +0700

    Add message CRUD and minor fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-// import "font-awesome/css/font-awesome.min.css";
+import "font-awesome/css/font-awesome.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import { Provider } from "react-redux";
