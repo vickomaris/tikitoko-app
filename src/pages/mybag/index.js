59d2a27fd8c4137888da0ba2949a1e53a451b5c4
unknown <alfat.pololo@gmail.com>
2022-11-23 20:13:47 +0700

    get data on mybag and checkout page

--- src/pages/mybag/index.js
+++ src/pages/mybag/index.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from "./mybag.module.css"
 import Navbar from '../../component/module/navbar'
+import axios from 'axios';
+import { useState } from 'react';
+import Button from '../../component/button';
 
 const Mybag = () => {
+    const [bagState, setBagState] = useState([]);
+    // const token = JSON.parse(localStorage.getItem("token"))
+    const token = localStorage.getItem("token")
+    const [sumarry, setSumarry] = useState([]);
+    
+
+    useEffect(() => {
+        axios.get(`http://localhost:4000/v1/cart/`,{
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          })
+        .then((response) => {
+            console.log(response.data)
+            setBagState(response.data)
+        })
+        .catch((err) => {
+            alert("Account not found")
+            console.log(err);
+        })
+    }, []) 
+
   return (
     <>
     <Navbar/>
@@ -10,7 +35,7 @@ const Mybag = () => {
                 <div className="container">
                     <h1 className={styles["title"]}>My Bag</h1>
                     <div className="row">
-                        <div className="col-sm-7 mb-4">
+                            <div className="col-sm-7 mb-4">
                         <div className={styles["card"]}>
                                 <div className="card-body px-4 py-3">
                                     <div className="flex d-flex align-items-center">
@@ -31,33 +56,38 @@ const Mybag = () => {
                                 </div>
                         </div>
                         <div className="mt-3">
-                                <div className={styles["card"]}>
+                        {
+                        bagState.data ?
+                        bagState.data.map((item, index) => (
+                                <div className={`mb-3 ${styles["card"]}`}>
                                     <div className="card-body px-4 py-4">
                                         <div className={styles["product"]}>
                                             <div className="d-flex align-items-center">
                                                 <input className={styles["check"]} type="checkbox" />
-                                                <img className={styles["product-img"]} src={require('../../assets/Mask Group.png')} alt="suite" />
+                                                <img className={styles["product-img"]} src={item.image} alt="suite" />
                                                 <div className={styles["brand"]}>
-                                                    <h5>Men's formal suit - Black</h5>
-                                                    <p>Zalora</p>
+                                                    <h5>{item.name}</h5>
+                                                    <p>{item.seller_name}</p>
                                                 </div>
                                             </div>
                                             <div className={`flex d-flex justify-content-center ${styles['count-product']}`}>
                                                 <button className={styles["circle"]}>-</button>
-                                                <p className={styles["sum"]}>1</p>
+                                                <p className={styles["sum"]}>{item.qty}</p>
                                                 <button className={styles["circle"]}>+</button>
                                             </div>
-                                            <p className={styles["price"]}>$ 20.0</p>
+                                            <p className={styles["price"]}>Rp{item.price}</p>
                                         </div>
                                     </div>
                                 </div>
+                                                        )) : ""
+                                                    }
                             </div>
                     </div>
                     <div className="col-sm-5 mb-4">
                         <div className={styles["card"]}>
                             <div className="card-body-right p-4">
                                 <h3 className={styles["card-summary"]}>Shopping summary</h3>
-                                <h5 className={styles["price"]}>Total price<span className="d-flex justify-content-end">$20</span></h5>
+                                <h5 className={styles["price"]}>Total price<span className="d-flex justify-content-end">Rp15000</span></h5>
                                 <button className={`${styles['buy']}`}>Buy</button>
                             </div>
                         </div>
